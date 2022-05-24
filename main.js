caf3a7611db77ee3a8b50e9c5e56f0bba8975a93
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-24 16:55:02 -0500

    Add operate fucntion

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let operatorState = false;
-let num1 = 0;
+let curNum = 0;
+let prevNum = undefined;
 let operator = undefined;
 
 function main() {
   createListeners();
-
-
-
 }
 
 function createListeners() {
@@ -15,45 +13,75 @@ function createListeners() {
   clearButton.addEventListener("click", clearDisplay);
 
   // create number and operator button listeners
-  const inputButtons = document.querySelectorAll(".num-button");
-  inputButtons.forEach((button) => {
+  const numButtons = document.querySelectorAll(".num-button");
+  numButtons.forEach((button) => {
     button.addEventListener("click", () => {
       onNumInput(button.getAttribute("data-value"));
     });
   });
+
+  const opButtons = document.querySelectorAll(".operator-button");
+  opButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      onOperatorInput(button.getAttribute("data-value"));
+    });
+  });
 }
 
-// when the user inputs a number
+// call when the user inputs a number
 function onNumInput(val) {
-  if (operatorState == false) {
-    num1.push(val);
-    displayInput(num1);
-  }
-
-  else {
-    operate(num1, operator, val);
-  }
+  console.debug("onNumInput()");
+  // update current number
+  if (curNum != 0) curNum = "" + curNum + val;
+  else curNum = val;
 }
 
+// call when the user inputs an operator
 function onOperatorInput(val) {
-
+  console.debug("onOperatorInput()");
+  // no previous expression
+  if (prevNum == undefined) {
+    prevNum = curNum;
+    curNum = 0;
+    operator = val;
+  }
+  else {
+    prevNum = operate(prevNum, operator, curNum);
+    curNum = 0;
+    operator = val;
+  }
+  displayInput(curNum);
 }
+
 
 // add passed argument to display
 function displayInput(input) {
   const display = document.querySelector(".display-input");
-  display.value += input;
+  display.value = input;
 }
 
 // clear display
 function clearDisplay() {
   const display = document.querySelector(".display-input");
   display.value = "";
+  num1 = 0;
 }
 
 // evaluate expression
 function operate(num1, operator, num2) {
-  console.log("todo");
+  alert("operate!");
+  switch(operator) {
+    case "÷":
+      return num1 / num2;
+    case "×":
+      return num1 * num2;
+    case "−":
+      return num1 - num2;
+    case "+":
+      return num1 + num2;
+    default:
+      return 0;
+  }
 }
 
 main();
